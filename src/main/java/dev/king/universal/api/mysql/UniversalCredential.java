37d300c7fb3adc012dbaded535db9f9a2887f9d6
zkingboos <kingboosoficial@gmail.com>
2020-12-30 14:28:42 -0300

    Added UniversalCredential#fromConfiguration method (working via api 1.16.2-R0.1)

--- src/main/java/dev/king/universal/api/mysql/UniversalCredential.java
+++ src/main/java/dev/king/universal/api/mysql/UniversalCredential.java
@@ -6,7 +6,9 @@ package dev.king.universal.api.mysql;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.configuration.ConfigurationSection;
 
 /**
  * The credential security login to authenticate with mysql
@@ -21,4 +23,13 @@ public final class UniversalCredential {
     private final String database;
     private final String user;
     private final String password;
+
+    public static UniversalCredential fromConfiguration(@NonNull ConfigurationSection section) {
+        return builder()
+          .hostname(section.getString("hostname"))
+          .database(section.getString("database"))
+          .user(section.getString("user"))
+          .password(section.getString("password"))
+          .build();
+    }
 }
