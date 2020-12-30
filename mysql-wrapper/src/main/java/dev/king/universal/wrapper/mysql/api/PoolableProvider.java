5dbaf9ed16cd76e7eaf5b3fc7239201794567af6
zkingboos <kingboosoficial@gmail.com>
2020-12-30 16:19:23 -0300

    Portable to gradle finished (i think)

---
+++ mysql-wrapper/src/main/java/dev/king/universal/wrapper/mysql/api/PoolableProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020 yking-projects
+ */
+
+package dev.king.universal.wrapper.mysql.api;
+
+import com.zaxxer.hikari.HikariDataSource;
+import dev.king.universal.shared.api.credential.UniversalCredential;
+import lombok.NonNull;
+
+import java.sql.SQLException;
+
+/**
+ * The pool of connection, usefully performance to big queries
+ */
+public interface PoolableProvider {
+
+    /**
+     * Gets the mysql connection
+     *
+     * @param credentials    used to login into mysql
+     * @param maxConnections passed in MysqlProvider constructor
+     * @return returns an DataSource object, that you can manage it
+     * @throws SQLException anything can break here, don't worry.
+     */
+    HikariDataSource obtainDataSource(@NonNull UniversalCredential credentials, int maxConnections) throws SQLException;
+}
