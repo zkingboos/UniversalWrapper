8e832c4885a176945c2257f68cbc095817e2afcf
zkingboos <kingboosoficial@gmail.com>
2021-04-13 02:24:14 -0300

    @EqualsAndHashCode(callSuper = true) cause an unexpect StackOverflow

--- mysql-wrapper-bukkit/src/main/java/dev/king/universal/wrapper/mysql/MySQLProvider.java
+++ mysql-wrapper-bukkit/src/main/java/dev/king/universal/wrapper/mysql/MySQLProvider.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Objects;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 public class MySQLProvider extends DefaultSQLSupport {
 
     private final UniversalCredential credential;
